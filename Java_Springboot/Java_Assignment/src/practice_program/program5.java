eed67edf05b151eed57b71a47fa89e9a5c83f89e
Tejastejas19 <tejas.parvam@gmail.com>
2026-03-17 12:42:43 +0530

    updated work

--- Java_Springboot/Java_Assignment/src/practice_program/program5.java
+++ Java_Springboot/Java_Assignment/src/practice_program/program5.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class program5 {
 	public static void main(String[] args) {
 		
-		Scanner sc = new Scanner(system.in); 
+		Scanner sc = new Scanner(System.in); 
 	}
 	
 }
